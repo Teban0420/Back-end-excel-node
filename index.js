6461aa1acdeddc0156adf5385e6de7903899b26f
Esteban Giraldo Jaramillo <giraldoesteban044@gmail.com>
2023-10-10 13:44:55 -0500

    cors y variables de entorno

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express') 
 const routes = require('./routes/index.js') 
 const db  = require('./config/db.js') 
+const cors = require('cors')
+require('dotenv').config({path: '.env'})
 
 // conexion db
 try {    
@@ -14,12 +16,49 @@ try {
 // servidor
 const app  = express()
 
+app.use((req, res, next) => {
+
+    res.header('Access-Control-Allow-Origin', '*');
+    
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    
+    next();
+    
+});
+
 // rutas del server
 app.use('/', routes())
 
+const whiteList = [process.env.FRONTEND_URL]
+
+const CorsOptions = {
+    origin: (origin, callback) => {
+        // revisar si la peticion viene de un servidor que esta en la lista blanca
+        const existe = whiteList.some( dominio => dominio === origin)
+        if(existe){
+            callback(null, true)
+        }
+        else{
+            callback(new Error('No permitido'))
+        }
+    }
+}
+
+// habilitar cors
+app.use(cors( CorsOptions ))
+// app.use(cors(  ))
+
+const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 5000
 
 // puerto
-app.listen(5000)
+app.listen(port, host, () => {
+    console.log('EL servidor funciona')
+})
 
  
 
